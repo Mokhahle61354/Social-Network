c0cd6d0b9e628484b85337c41ff60fb2eb3734e3
Mokhahle Thabo <mokhahle.thabo@gmail.com>
2021-01-16 01:07:04 +0300

    last_update

--- app/analysis.py
+++ app/analysis.py
@@ -1,4 +1,4 @@
-def recommed_by_id(id:int):
+def recommed_by_id(id: int):
     cypher = f"""
 
     MATCH
@@ -24,16 +24,137 @@ def recommed_by_id(id:int):
         r.rating>4
         AND (sm)-[:IN_GENRE]-(g)
         AND NOT (sm)-[:RATED]-(u)
-    RETURN sm 
+    RETURN u, g,sm, su
     LIMIT 25
     """
 
     return cypher
 
 
-class Movie:
-    
-    def __init__(self, data):
-        self.data = data[0]["sm"].nodes[0]
+def recommed_by_genre(id: int, genre: str):
+    cypher = f"""
+
+    MATCH
+        (hrm:Movie)<-[r:RATED]-(u:User)
+    WHERE
+        u.userId = "{id}"
+        And r.rating>4
+    WITH u,hrm LIMIT 25
+
+    MATCH
+        (g:Genre)-[:IN_GENRE]-(hrm)
+    WHERE
+        g.name = "{genre}"
+    WITH u,hrm,g
+
+    MATCH
+        (su:User)-[r:RATED]-(hrm)
+    WHERE
+        r.rating>4
+    WITH u,su,g
+
+    MATCH
+        (sm:Movie)<-[r:RATED]-(su)
+    WHERE
+        r.rating>4
+        AND (sm)-[:IN_GENRE]-(g)
+        AND NOT (sm)-[:RATED]-(u)
+    RETURN u, g,sm, su
+    LIMIT 25
+    """
+
+    return cypher
+
+
+def get_user(id: int):
+    cypher = f"""
+    MATCH 
+        (u:User)
+    WHERE
+        u.userId = "{id}"
+    RETURN u.name
+    """
+
+    print(cypher)
+    pass
+
+
+from py2neo import Graph
+
+from PIL import Image
+import requests
+from io import BytesIO
+import numpy as np
+
+
+class Recommend:
+    def __init__(self, movies_graph, user_id):
+        self.movies_graph = movies_graph
+        self.data = self.run_cypher(cypher=recommed_by_id(id=user_id))
+        self.user_id = user_id
+        self.user_name = self.data[0]["u"].nodes[0].get("name")
         pass
 
+    def byGenre(self, genre):
+        self.data = self.run_cypher(
+            cypher=recommed_by_genre(id=self.user_id, genre=genre)
+        )
+        return
+
+    def get_all_genres(self):
+        cyp = "MATCH (n:Genre) RETURN n LIMIT 25"
+        genres = self.run_cypher(cypher=cyp)
+        lst_genres = [
+            gnr["name"] for idx in range(len(genres)) for gnr in genres[idx]["n"].nodes
+        ]
+        return lst_genres
+
+    def run_cypher(self, cypher):
+        results = self.movies_graph.run(cypher=cypher)
+        data = results.data().copy()
+        return data
+
+    def SimilarUsers(self):
+        similar_users = []
+        id_set = []
+        for index in range(len(self.data)):
+            # name = self.data[index]["su"].nodes[0].get("name")
+            user = self.data[index]["su"].nodes[0]
+            similar_users.append(user)
+            pass
+        # get unique users from list
+        similar_users = list({p["userId"]: p for p in similar_users}.values())
+        return similar_users  # np.unique(similar_users)
+
+    # @st.cache
+    def getMovieInfo(self, index=0):
+        released = self.data[index]["sm"].nodes[0].get("released")
+        runtime = self.data[index]["sm"].nodes[0].get("runtime")
+        movieId = self.data[index]["sm"].nodes[0].get("movieID")
+        rating = self.data[index]["sm"].nodes[0].get("imbdRating")
+        year = self.data[index]["sm"].nodes[0].get("year")
+        title = self.data[index]["sm"].nodes[0].get("title")
+        # Image
+        url = self.data[index]["sm"].nodes[0].get("poster")
+        response = requests.get(url)
+        poster = Image.open(BytesIO(response.content))
+        su_id = self.data[index]["su"].nodes[0].get("userId")
+        genre = self.data[index]["g"].nodes[0].get("name")
+
+        return dict(
+            movieId=movieId,
+            title=title,
+            poster=poster,
+            rating=rating,
+            year=year,
+            runtime=runtime,
+            released=released,
+            su_id=su_id,  # similar user ID
+            genre=genre,
+        )
+
+
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        pass
--- view.py
+++ view.py
@@ -1,98 +1,157 @@
 # Aura queries use an encrypted connection using the "neo4j+s" URI scheme
-bolt_url = "bolt://34.229.69.148:33078" #"%%BOLT_URL_PLACEHOLDER%%"
-user = "neo4j" #"<Username for Neo4j Aura database>"
-password = "metal-technicians-morale" #"<Password for Neo4j Aura database>"
+bolt_url = "bolt://34.229.69.148:33078"  # "%%BOLT_URL_PLACEHOLDER%%"
+user = "neo4j"  # "<Username for Neo4j Aura database>"
+password = "metal-technicians-morale"  # "<Password for Neo4j Aura database>"
 
+import pandas as pd
 from py2neo import Graph
 import streamlit as st
 from PIL import Image
-from app.analysis import recommed_by_id
+from app.analysis import Recommend, get_user, recommed_by_id
 
 from PIL import Image
 import requests
 from io import BytesIO
 
-recommend = st.sidebar.checkbox("Recommender system")
+st.title("Movie Recommender System")
+"""
+Thabo Mokhahle 1738085\n
+Tools used:\n
+- Neo4j with py2neo\n
+- UI : streamlit
+- Dataset : https://sandbox.neo4j.com/?ref=hcard  -> new project -> "Recommendation"
+    - For new bolt password and ip
 
 
-if recommend:
-    st.title('Movie Recommender System')
-    str_url = "bolt://neo4j:switch-shortage-shell@34.229.69.148:33108"
+```
+Dependecies:
+    pip install neo4j
+    pip install -U pylint --user
+    pip install neo4j-driver
+    pip install streamlit
+    pip install --upgrade py2neo
+    pandas
+
+Run App:
+ - streamlit run view.py
+
+user provided env:
+
+
+```
+"""
+
+chbx_recommend = st.sidebar.checkbox("Recommender system")
+
+
+if chbx_recommend:
+    """
+    #### _Connect to Neo4j_
+    """
+    col_bolt_ip, col_password = st.beta_columns(2)
+    txt_bolt_ip = col_bolt_ip.text_input("Neo4j bolt ip:", value="100.25.182.248:33004")
+    txt_bolt_password = col_password.text_input(
+        "Neo4j bolt Password :", value="dawns-decoders-pull"
+    )
+    str_url = f"bolt://neo4j:{txt_bolt_password}@{txt_bolt_ip}"
     movies_index = 0
     movies_graph = Graph(str_url)
 
+    """
+    #### _User and filter inputs_
+    """
+    user_id = st.text_input("User Id:", 2)
+    obj_recommend = Recommend(movies_graph=movies_graph, user_id=user_id)
+    all_genres = obj_recommend.get_all_genres()
 
-    user_input = st.text_input("User Id:", 0)
-    cypher = recommed_by_id(id=user_input)
-    # f"""
-    # user id {user_input}
+    col_genre, col_slt_genre = st.beta_columns(2)
+    # is_by_genre = False
+    with col_genre:
+        slt_genre = st.selectbox(label="Genre: ", options=all_genres)
+        is_by_genre = col_slt_genre.checkbox("Activate genre")
+        if is_by_genre:
+            obj_recommend.byGenre(genre=slt_genre)
+            col_slt_genre.write("Will filter by selected genre!!!")
 
-    # {cypher}
-    # """
-    movies_recommended = movies_graph.run(cypher=cypher)
-    data = movies_recommended.data().copy()
-    @st.cache
-    def getTitle(index=0):
-        global data
-        return data[index]["sm"].nodes[0].get("title")
+    f"""
+    ### __Desplay recommended movies__
+    ```
+    User Name: {obj_recommend.user_name}
+    User ID: {obj_recommend.user_id}
+    ```
+    """
 
-    @st.cache
-    def getImage(index=0):
-        global data
-        url = data[index]["sm"].nodes[0].get("poster")
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content))
-        return img
-    #st.write(movies_recommended)
-    count = -3 # reson for -3 is to make sure slide corrolate with cols
-    for _ in data:
-        count=count+1
+    count = -3  # reson for -3 is to make sure slide corrolate with cols
+    for _ in obj_recommend.data:
+        count = count + 1
     # Pan through.
-    page = st.slider("Page: ", min_value=0, max_value=count, value=0, step=1, format=None, key=None)
+    page = st.slider(
+        "Page: ", min_value=0, max_value=count, value=0, step=1, format=None, key=None
+    )
     movies_index = page
-    # col_back, col_page, col_forward = st.beta_columns(3)
 
-    # goback = col_back.button("Back")
-    # gofoward = col_forward.button("Next")
-
-    # @st.cache
-    # def pan():
-    #     global movies_index
-    #     if goback:
-    #         movies_index = movies_index-1
-    #         print(movies_index)
-    #     elif gofoward:
-    #         movies_index = movies_index+1
-    #         print(movies_index)
-
-    # num_page = col_page.write(movies_index)
-
-    # pan()
-    """
-    ## __Desplay recommended movies__
-    """
     col1, col2, col3 = st.beta_columns(3)
-
     with col1:
         idx = movies_index
-        st.header(f"""{getTitle(index=idx)}""")
-        st.image(getImage(index=idx), use_column_width=True)
+        rcm_movie = obj_recommend.getMovieInfo(index=idx)
+        st.header(f"""{rcm_movie["title"]}""")
+        st.image(rcm_movie["poster"], use_column_width=True)
+        # st.write(f"""Similar userID: {rcm_movie["su_id"]}""")
+        {
+            key: rcm_movie.get(key)
+            for key in ["genre", "year", "runtime", "released", "su_id"]
+        }
 
     with col2:
         idx = movies_index + 1
-        st.header(f"""{getTitle(index=idx)}""")
-        st.image(getImage(index=idx), use_column_width=True)
+        rcm_movie = obj_recommend.getMovieInfo(index=idx)
+        st.header(f"""{rcm_movie["title"]}""")
+        st.image(rcm_movie["poster"], use_column_width=True)
+        # st.write(f"""Similar userID: {rcm_movie["su_id"]}""")
+        {
+            key: rcm_movie.get(key)
+            for key in ["genre", "year", "runtime", "released", "su_id"]
+        }
 
     with col3:
         idx = movies_index + 2
-        st.header(f"""{getTitle(index=idx)}""")
-        st.image(getImage(index=idx), use_column_width=True)
+        rcm_movie = obj_recommend.getMovieInfo(index=idx)
+        st.header(f"""{rcm_movie["title"]}""")
+        st.image(rcm_movie["poster"], use_column_width=True)
+        # st.write(f"""Similar userID: {rcm_movie["su_id"]}""")
+        {
+            key: rcm_movie.get(key)
+            for key in ["genre", "year", "runtime", "released", "su_id"]
+        }
         movies_index = movies_index + 2
+        pass
 
+    col_sm_users, col_sm_info = st.beta_columns(2)
 
-    st.write(f"""
-    {data}
+    with col_sm_users:
+        # change col_sm_info here
+        pass
+
+st.markdown(
     """
-    )
+    ==========================================================================
+    # Improvement plan:
 
+    ### Future Milestones
+    - :computer: GraphQl server
+        - separate app from GUI
+        - Access neo4j server
+        
+        Using https://tartiflette.io/ or https://ariadnegraphql.org/ :
+        - tartiflette  -> _async
+        - ariadnegraphql
 
+    - :fire: :fire: Neo4j
+        - Node and Graph management
+        - 
+
+    - :confused: Intergration of the algorithms from:
+        - Graph Data Science in neo4j
+        - https://neo4j.com/docs/graph-data-science/1.4/management-ops/
+    """
+)
